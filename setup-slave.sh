fb27d17511fddc4c67d72d95af8ef58e81f18c38
Qing Zheng <zhengq@cs.cmu.edu>
2017-03-13 22:03:34 -0400

    update vm packages

--- setup-slave.sh
+++ setup-slave.sh
@@ -129,8 +129,7 @@ sudo service lxc start
 function create_vm() {
 ### @param rack_id, host_id, ip, mem, ncpus ###
     VM_NAME=`echo r"$1"h"$2"`
-    sudo lxc-create -n $VM_NAME -t ubuntu -- --packages \
-        "vim,curl,wget,git,default-jre,sysbench"
+    sudo lxc-create -n $VM_NAME -t ubuntu -- --packages "sysbench"
     sudo sed -i "/lxc.network.ipv4 =/c lxc.network.ipv4 = $3" \
         /mnt/$VM_NAME/config
     sudo sed -i "/lxc.cgroup.memory.max_usage_in_bytes =/c lxc.cgroup.memory.max_usage_in_bytes = $4" \
