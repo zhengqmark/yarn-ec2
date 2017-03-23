69efabb99a7e114a65b6c8f4e0e6a7e79372b795
Qing Zheng <zhengq@cs.cmu.edu>
2017-03-22 23:33:04 -0400

    fix env bugs

--- setup.sh
+++ setup.sh
@@ -91,7 +91,8 @@ wait
 echo "Setting up cluster nodes..." > /dev/null
 $PDSH -w ^all-nodes $HOME/share/yarn-ec2/setup-slave.sh \
     2>&1 | tee $HOME/tmp/setup-slaves.log
-env HADOOP_CONF_DIR=/tmp/hd/conf HADOOP_LOG_DIR=/tmp/hd/logs \
+env JAVA_HOME=/usr/lib/jvm/default-java HADOOP_HOME=/usr/local/hd \
+    HADOOP_CONF_DIR=/tmp/hd/conf HADOOP_LOG_DIR=/tmp/hd/logs HADOOP_PID_DIR=/tmp \
     /tmp/hd/bin/hdfs namenode -format -force
 $PDSH -w ^all-nodes $HOME/share/yarn-ec2/start-slave.sh \
     2>&1 | tee $HOME/tmp/start-slaves.log
